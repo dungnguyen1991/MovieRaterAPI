e695bed9fe2de0e4edcc4bb8c34df7da5fbdb57b
dungnguyen1991 <dungnguyendeveloper1991@gmail.com>
2020-06-04 20:30:28 +0700

    django_heroku

--- MovieRaterApi/settings.py
+++ MovieRaterApi/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 from decouple import config
 from dj_database_url import parse as dburl
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -137,3 +138,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+django_heroku.settings(locals())
